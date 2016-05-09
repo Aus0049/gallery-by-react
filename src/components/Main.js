2be1ca54f090d9ffe3f387c5e9a8f47b1bd89d24
= <=>
2016-05-09 13:54:31 +0800

    work done

--- src/components/Main.js
+++ src/components/Main.js
@@ -23,13 +23,28 @@ imageDatas = (function getImageURL(imageDataArr){
 
 //每个图片是一个类
 var ImgFigure = React.createClass({
+
+    handleClick:function(e){
+
+        if(this.props.arrange.isCenter){
+            this.props.inverse();
+        }else {
+            this.props.center();
+        }
+        
+
+        e.stopPropagation();
+        e.preventDefault();
+
+    },
+
     render: function(){
 
         //渲染位置
         var styleObj = {};
 
-        if(this.props.arrange){
-            styleObj = this.props.arrange;
+        if(this.props.arrange.style){
+            styleObj = this.props.arrange.style;
         }
 
         //处理浏览器兼容前缀
@@ -39,13 +54,52 @@ var ImgFigure = React.createClass({
             }.bind(this));
         }
 
+        //处理翻转 类名控制
+        var imgFigureClassName = "img-figure";
+        imgFigureClassName += this.props.arrange.isInverse ? ' is-inverse' : '';
+
         return (
-            <figure className="img-figure" style={styleObj}>
+            <figure className={imgFigureClassName} style={styleObj} onClick={this.handleClick}>
                 <img src={this.props.data.imgURL} alt={this.props.data.title}/>
                 <figcaption>
                     <h2 className="img-title">{this.props.data.title}</h2>
+                    <div className="img-back" onClick={this.handleClick}>
+                        {this.props.data.desc}
+                    </div>
                 </figcaption>
             </figure>
+        );
+    }
+});
+
+//类
+var ControllerUnit = React.createClass({
+
+    handleClick:function(e){
+
+        if(this.props.arrange.isCenter){
+            this.props.inverse();
+        }else {
+            this.props.center();
+        }
+
+        e.stopPropagation();
+        e.preventDefault();
+    },
+
+    render: function(){
+
+        var controllerUnitClassName = "controller-unit";
+        if(this.props.arrange.isCenter){
+            controllerUnitClassName += " is-center";
+
+            if(this.props.arrange.isInverse){
+                controllerUnitClassName += " is-inverse";
+            }
+        }
+
+        return (
+            <span className={controllerUnitClassName} onClick={this.handleClick}></span>
         );
     }
 });
@@ -58,6 +112,25 @@ var AppComponent = React.createClass({
         rangeInRightX:[0,0],
         rangeInTopY:[0,0],
         ranfeInBottomY:[0,0]
+    },
+    //各个元素尺寸
+    HeightAndWidth:{
+        halfStageW:0,
+        halfImgW:0,
+        halfStageH:0,
+        halfImgH:0
+    },
+
+    inverse:function(index){
+        return function(){
+            var imgsArrangeArr = this.state.imgsArrangeArr;
+
+            imgsArrangeArr[index].isInverse = !imgsArrangeArr[index].isInverse;
+
+            this.setState({
+                imgsArrangeArr:imgsArrangeArr
+            });
+        }.bind(this);
     },
 
     //初始化状态
@@ -99,14 +172,24 @@ var AppComponent = React.createClass({
         this.Constant.ranfeInBottomY[0] = stageH - halfImgH * 3;
         this.Constant.ranfeInBottomY[1] = stageH - halfImgH;
 
+        //设置尺寸
+        this.HeightAndWidth.halfStageW = halfStageW;
+        this.HeightAndWidth.halfImgW = halfImgW;
+        this.HeightAndWidth.halfStageH = halfStageH;
+        this.HeightAndWidth.halfImgH = halfImgH;
+
+        this.rearange(0);
+    },
+
+    rearange:function(index){
         //每个图片的位置数组
         var positionArr = [];
 
         for(var i = 0, j = imageDatas.length; i < j; i++ ){
-            if(i == 0){
-                positionArr.push({left:halfStageW - halfImgW, top:halfStageH - halfImgH})
+            if(i == index){
+                positionArr.push({style:{left:this.HeightAndWidth.halfStageW - this.HeightAndWidth.halfImgW, top:this.HeightAndWidth.halfStageH - this.HeightAndWidth.halfImgH},isInverse:false,isCenter:true})
             }
-            positionArr.push(this.getRandomTopAndLeftInRange(i))
+            positionArr.push({style:this.getRandomTopAndLeftInRange(i),isInverse:false,isCenter:false})
         }
 
         //更新状态 触发渲染
@@ -155,6 +238,12 @@ var AppComponent = React.createClass({
         return 'rotate(' + deg + 'deg)';
     },
 
+    center:function(index){
+        return function(){
+            this.rearange(index);
+        }.bind(this);
+    },
+
     render:function(){
 
         var controllerUnits = [];
@@ -164,13 +253,18 @@ var AppComponent = React.createClass({
 
             if(!this.state.imgsArrangeArr[index]){
                 this.state.imgsArrangeArr[index] = {
-                    left:0,
-                    top:0,
-                    transform:'rotate(0deg)'
+                    style:{
+                        left:0,
+                        top:0,
+                        transform:'rotate(0deg)'
+                    },
+                    isInverse:false,
+                    isCenter:false
                 };
             }
 
-            imgFigures.push(<ImgFigure data={value} ref={'imgFigure' + index} arrange={this.state.imgsArrangeArr[index]}/>)
+            imgFigures.push(<ImgFigure data={value} ref={'imgFigure' + index} arrange={this.state.imgsArrangeArr[index]} inverse={this.inverse(index)} center={this.center(index)}/>)
+            controllerUnits.push(<ControllerUnit arrange={this.state.imgsArrangeArr[index]} inverse={this.inverse(index)} center={this.center(index)} />);
         }.bind(this));
 
         return (
